9307ac7a200511711a4280a89458388afbba23a3
Toheed <toheedsk58@gmail.com>
2023-11-13 21:08:44 +0530

    add demo component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/login/login.component';
 import { pageNotFoundComponent } from './modules/pageNotFound/pageNotFound.component';
+import { DemoComponent } from './modules/demo/demo.component';
 
 const routes: Routes = [
   { path: 'login' , component: LoginComponent },
+  { path : 'demo' , component: DemoComponent},
   { path: 'home', loadChildren: () => import('./modules/home/home.module').then( m => m.HomeModule)},
   { path: 'list',  loadChildren : () => import('./modules/list/list.module').then(m => m.ListModule)},
 
---
+++ src/app/modules/demo/demo.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormGroup} from '@angular/forms'
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.scss']
+})
+export class DemoComponent {
+
+  form : FormGroup;
+
+  constructor(){
+    this.form = new FormGroup({
+
+    })
+  }
+}
