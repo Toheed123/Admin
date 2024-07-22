b3785398abff1c8fa54b95d3fb9afddecb03b949
Toheed <toheedsk58@gmail.com>
2024-07-22 23:23:10 +0530

    Login Changes

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -9,6 +9,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent {
 
   loginForm : FormGroup ;
+  forgotPasswordForm : FormGroup;
+  selectedTabIndex : number;
+  tabContainer = {
+    forgotPassword : {
+      tabIndex : 1,
+      tabName : "Forgot Password"
+    },
+    signIn : {
+      tabIndex : 2,
+      tabName : "Sing In"
+    }
+}
 
   constructor(
     private formBuilder : FormBuilder,
@@ -17,8 +29,23 @@ export class LoginComponent {
       userName:[[],[Validators.required]],
       password: [[], [Validators.required]]
     })
+    this.forgotPasswordForm = this.formBuilder.group({
+      email:[[],[Validators.required, Validators.email]],
+    })
   }
   ngOnInIt(){
 
+  }
+
+  get loginFormControls(){return this.loginForm.controls}
+
+  get forgotPasswordFormControls(){return this.forgotPasswordForm.controls}
+
+  changeTab(tabIndex : any){
+    this.selectedTabIndex = tabIndex
+  }
+
+  cancel(){
+    this.selectedTabIndex = 0
   }
 }
